873e1e2493fd16f896b1dccd859235045dd865f9
willian honorio diaz torres <whdiaztorres@gmail.com>
2022-05-17 22:04:08 -0300

    DTO and inplements

--- src/main/java/com/whdiaztorres/practicamongo/resources/UserResource.java
+++ src/main/java/com/whdiaztorres/practicamongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.whdiaztorres.practicamongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,20 +10,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.whdiaztorres.practicamongo.domain.User;
+import com.whdiaztorres.practicamongo.dto.UserDTO;
 import com.whdiaztorres.practicamongo.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
-		
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+
 	}
 
 }
