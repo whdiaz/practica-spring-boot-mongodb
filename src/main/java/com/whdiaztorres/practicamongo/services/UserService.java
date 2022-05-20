213bec1394edda3704cafeab21c8a6260c399881
willian honorio diaz torres <whdiaztorres@gmail.com>
2022-05-19 23:50:12 -0300

    tabalhando o DTO

--- src/main/java/com/whdiaztorres/practicamongo/services/UserService.java
+++ src/main/java/com/whdiaztorres/practicamongo/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.whdiaztorres.practicamongo.Repository.UserRepository;
 import com.whdiaztorres.practicamongo.domain.User;
+import com.whdiaztorres.practicamongo.dto.UserDTO;
 import com.whdiaztorres.practicamongo.services.exception.ObjectNotFoundException;
 
 @Service
@@ -25,5 +26,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+    
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
